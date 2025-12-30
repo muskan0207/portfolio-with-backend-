8a86902b45722e448ae15e0bea4d520c9777f313
muskan0207 <muskan0207@gmial.com>
2025-12-31 00:52:45 +0530

    remove webvital

--- portfolioo/src/reportWebVitals.js
+++
@@ -1,8 +0,0 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    // Remove web-vitals import to fix Vercel build
-    console.log('Performance monitoring disabled for production build');
-  }
-};
-
-export default reportWebVitals;
